ae67f8f5c1c45bc44dbee78ed1c568442fcaf52a
Ammar <ammar.yassr.33@gmail.com>
2024-02-04 21:41:15 +0200

    Add task 8

---
+++ tasks/task 08/index.js
@@ -0,0 +1,8 @@
+/*
+// -- Manage Users API (CRUD) --
+GET /users --> send all users to client
+GET /users/:id --> send user with id to client
+POST /users --> create new user
+DELETE /users/:id --> delete user with id
+PATCH /users/:id --> update user with id
+*/
